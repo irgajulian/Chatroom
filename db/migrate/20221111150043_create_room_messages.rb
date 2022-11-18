e9f396b8831a27cbbd5ec75c336a44e70677d482
irgajulian <diputra184@gmail.com>
2022-11-18 11:56:15 +0700

    init
    init

--- db/migrate/20221111150043_create_room_messages.rb
+++ db/migrate/20221111150043_create_room_messages.rb
@@ -1,4 +1,4 @@
-class CreateRoomMessages < ActiveRecord::Migration[5.2]
+class CreateRoomMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :room_messages do |t|
       t.references :room, foreign_key: true
